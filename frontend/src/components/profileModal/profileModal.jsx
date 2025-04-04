17bd1262b1ec77598c8b049493137ff41c76ec0d
blinovapolina <blinovapolina06@yandex.ru>
2025-04-05 01:49:02 +0300

    changed svg

--- frontend/src/components/profileModal/profileModal.jsx
+++ frontend/src/components/profileModal/profileModal.jsx
@@ -11,7 +11,7 @@ export const ProfileModal = ({name,surname,email,nick}) => {
                 <div className="lineInfoProfileModal">Логин: {nick}</div>
                 </div>
 <div className="lineFavouriteProfileModal">Избранное <svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M12.43 0.929932L18.5 6.99993L12.43 13.0699M1.5 6.99993H18.33" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M12.43 0.929932L18.5 6.99993L12.43 13.0699M1.5 6.99993H18.33" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
 </svg></div>
 <div className="logoutTextProfileModal">
 Выйти из аккаунта
