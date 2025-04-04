dd27f9197eca0052c573e6cdd50888532afec3b5
blinovapolina <blinovapolina06@yandex.ru>
2025-04-05 01:30:07 +0300

    changed icon and svg

--- movie_app/frontend/src/components/profileModal/profileModal.jsx
+++ movie_app/frontend/src/components/profileModal/profileModal.jsx
@@ -9,8 +9,8 @@ export const ProfileModal = ({name,surname,email,nick}) => {
                 <div className="lineInfoProfileModal">Email: {email}</div>
                 <div className="lineInfoProfileModal">Логин: {nick}</div>
                 </div>
-<div className="lineFavouriteProfileModal">Избранное <svg width="26" height="14" viewBox="0 0 26 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M1.91663 7H24.0833M24.0833 7L13 1.75M24.0833 7L13 12.25" stroke="#D9D9D9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+<div className="lineFavouriteProfileModal">Избранное <svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12.43 0.929932L18.5 6.99993L12.43 13.0699M1.5 6.99993H18.33" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 </svg></div>
 <div className="logoutTextProfileModal">
 Выйти из аккаунта
